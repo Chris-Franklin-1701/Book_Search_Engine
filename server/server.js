9f79fdf673bfbc6c641b26c929104a6dba4a84a6
Chris.Franklin <cfrank306@gmail.com>
2021-12-09 09:12:05 -0600

    fixed server

--- server/server.js
+++ server/server.js
@@ -6,14 +6,16 @@ const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-await server.start();
-
-server.applyMiddleware({ app });
+async function startServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+  await server.start();
+  
+  server.applyMiddleware({ app });
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

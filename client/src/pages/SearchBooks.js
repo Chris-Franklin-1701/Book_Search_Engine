216873a4d0e4a83ae471bafa9177846f3ecbfa36
Chris.Franklin <cfrank306@gmail.com>
2021-12-09 01:03:29 -0600

    fixed mutations spelling

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 import { useMutation } from '@apollo/client'
 import Auth from '../utils/auth';
-import { searchGoogleBooks } from '../utils/mutatuions';
-import { SAVE_BOOK } from '../utils/mutatuions'
+import { searchGoogleBooks } from '../utils/mutations';
+import { SAVE_BOOK } from '../utils/mutations'
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {

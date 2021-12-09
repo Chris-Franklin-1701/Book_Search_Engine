007790c4f17869438555d4cc42db43a93062b7de
Chris.Franklin <cfrank306@gmail.com>
2021-12-09 01:42:29 -0600

    fixing heroku logs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -41,7 +41,7 @@ const typeDefs = gql`
     type Mutation {
         createUser(username: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!): Auth
-        saveBook(bookData: BookInput): User
+        saveBook(bookData: Input): User
         deleteBook(bookId: String!): User
     }
 `;
